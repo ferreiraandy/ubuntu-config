10e436d87363ac51d077d9ab1a9d388b7e129715
Anderson Ferreira <andyferreira92@gmail.com>
2019-08-07 17:16:51 -0300

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,18 +1,12 @@
 
 #!/usr/bin/env bash
-# https://developers.supportbee.com/blog/setting-up-cucumber-to-run-with-Chrome-on-Linux/
-# https://gist.github.com/curtismcmullan/7be1a8c1c841a9d8db2c
-# http://stackoverflow.com/questions/10792403/how-do-i-get-chrome-working-with-selenium-using-php-webdriver
-# http://stackoverflow.com/questions/26133486/how-to-specify-binary-path-for-remote-chromedriver-in-codeception
-# http://stackoverflow.com/questions/40262682/how-to-run-selenium-3-x-with-chrome-driver-through-terminal
-# http://askubuntu.com/questions/760085/how-do-you-install-google-chrome-on-ubuntu-16-04
 
 # Versions
 CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-SELENIUM_STANDALONE_VERSION=3.4.0
-SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
-# Remove existing downloads and binaries so we can start from scratch.
+# Removes from npm
+npm -g uninstall chromedriver
+
 sudo apt-get remove google-chrome-stable
 rm ~/selenium-server-standalone-*.jar
 rm ~/chromedriver_linux64.zip
@@ -21,7 +15,6 @@ sudo rm /usr/local/bin/selenium-server-standalone.jar
 
 # Install dependencies.
 sudo apt-get update
-sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
 # Install Chrome.
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
@@ -35,10 +28,4 @@ unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
 sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
-
-# Install Selenium.
-wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
-sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
-sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
-sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
+sudo chmod -R 755 /usr/local/bin/chromedriver
